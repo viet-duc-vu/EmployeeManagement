d7f23d4fa2116d3798b51eaffc990725ba905e14
Viet Vu <llv43@txstate.edu>
2024-02-18 14:42:05 -0600

    add column names

--- src/main/java/com/ducvu/EmployeeManagement/model/Employee.java
+++ src/main/java/com/ducvu/EmployeeManagement/model/Employee.java
@@ -1,5 +1,6 @@
 package com.ducvu.EmployeeManagement.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,11 +20,16 @@ import org.hibernate.annotations.NaturalId;
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     @NaturalId(mutable = true)
     private String email;
+    @Column(name = "department")
     private String department;
 
     public Employee(String firstName, String lastName, String email, String department) {
